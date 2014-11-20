f445d410995e8a48195d34294eb7dfe5abc01efe
Lauren Alexander <laurenpaigealexander@gmail.com>
2014-11-20 18:21:45 -0500

    add functions to import status, stations, and baseball data

--- read_prep.py
+++ read_prep.py
@@ -77,3 +77,133 @@ def weather(url):
 	weather_df['weekday'] = weather_df.datetime.map(lambda t: t.weekday())
 	
 	return weather_df
+	
+
+#####--------------- STATION STATUS -------------------#####
+
+"""
+Function
+--------
+status
+	Returns a dataframe with Hubway station status data for 2012 from 3 separate input csv files. 
+
+Parameters
+----------
+url1 : string
+    url of first csv file
+url2 : string
+    url of second csv file
+url3 : string
+    url of third csv file
+
+Returns
+-------
+a DataFrame
+	A pandas DataFrame containing station status data for 2012. Station status entries with capacity=0 are removed.
+
+Example
+-------
+>>> status('https://raw.githubusercontent.com/CS109Hubway/classp/master/data/stationstatus8_11to2_12_5min.csv',
+	'https://raw.githubusercontent.com/CS109Hubway/classp/master/data/stationstatus3_12to6_12_5min.csv',
+	'https://raw.githubusercontent.com/CS109Hubway/classp/master/data/stationstatus7_12to10_12_5min.csv')
+"""
+
+def status(url1,url2,url3):
+	#import CSVs
+	status1 = pd.read_csv(url1, header=None)
+	status2 = pd.read_csv(url2, header=None)
+	status3 = pd.read_csv(url3, header=None)
+	
+	#combine station status dataframes
+	status_df = pd.concat([status1,status2,status3])
+	status_df.columns = ['uniqueid', 'stationid', 'timestamp', 'nbBikes','nbEmptyDocks', 'capacity']
+	
+	#extract time and date fields from timestamp
+	status_df.timestamp = pd.to_datetime(status_df.timestamp)
+	status_df = status.loc[status_df.timestamp.map(lambda t: t.year) == 2012]
+	status_df = status.loc[status_df.capacity != 0]
+	status_df['hour'] =  status_df.timestamp.map(lambda t: t.hour)
+	status_df['minute'] = status_df.timestamp.map(lambda t: t.minute)
+	status_df.minute = status_df.minute+60*status.hour
+	status_df['month'] =  status_df.timestamp.map(lambda t: t.month)
+	status_df['daydate'] = status_df.timestamp.map(lambda t: t.date())
+	status_df['weekday'] =  status_df.timestamp.map(lambda t: t.weekday())
+	status_df.reset_index(inplace=True)
+
+	return status_df
+
+	
+#####--------------- STATIONS -------------------#####
+
+"""
+Function
+--------
+stations
+	Returns a dataframe with Hubway station data. 
+
+Parameters
+----------
+url : string
+    url of input csv file
+
+
+Returns
+-------
+a DataFrame
+	A pandas DataFrame containing station data.
+
+Example
+-------
+>>> stations('https://raw.githubusercontent.com/CS109Hubway/classp/master/data/hubway_stations.csv')
+"""
+
+def stations(url):
+
+	#read in station data
+	stations_df = pd.read_csv(url)	
+	
+	return stations_df
+
+
+#####--------------- BASEBALL -------------------#####
+	
+"""
+Function
+--------
+baseball
+	Returns a dataframe with Red Sox home games for 2012. 
+
+Parameters
+----------
+url : string
+    url of input csv file
+
+
+Returns
+-------
+a DataFrame
+	A pandas DataFrame containing Red Sox game times and durations.
+
+Example
+-------
+>>> baseball('https://raw.githubusercontent.com/CS109Hubway/classp/master/data/RedSox2012.csv')
+"""
+
+def baseball(url):
+	#read in baseball data
+	MLB = pd.read_csv(url, ', ')
+
+	#use date, year, time, and duration fields to generate start and end time stamps and drop temp fields
+	MLB['start_time']=pd.to_datetime(MLB['Date']+' '+MLB['Year'].astype(str)+ ' ' +MLB['Time'])
+	MLB['Durhour']=MLB['Duration'].apply(lambda x: x.split(':')[0]).astype(int)
+	MLB['Durmin']=MLB['Duration'].apply(lambda x: x.split(':')[1]).astype(int)
+	MLB['timedelta']=MLB['Durhour'].apply(lambda x: datetime.timedelta(hours=x))+MLB['Durmin'].apply(lambda x: datetime.timedelta(minutes=x))
+	MLB['end_time'] = MLB["start_time"]+MLB["timedelta"]
+	MLB['hour'] = MLB.start_time.map(lambda t: t.hour)
+	MLB['minute'] = MLB.start_time.map(lambda t: t.minute)
+	MLB['month'] = MLB.start_time.map(lambda t: t.month)
+	MLB['daydate'] = MLB.start_time.map(lambda t: t.date())
+	MLB['weekday'] = MLB.start_time.map(lambda t: t.weekday())
+	MLB.drop(['Durhour', 'Durmin', 'timedelta'], axis=1, inplace=True)
+
+	return MLB
